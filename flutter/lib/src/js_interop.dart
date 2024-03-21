d106130d51091f4026aa0f030a24607f1126b4eb
yudhishthir-siterecon <yudhishtir.rajawat@siterecon.ai>
2024-03-21 14:39:28 +0530

    Add working code - Embed raw fluter app in react

---
+++ flutter/lib/src/js_interop.dart
@@ -0,0 +1,7 @@
+/// Exposes useful functions to interop with JS from our Flutter app.
+library;
+
+export 'js_interop/counter_state_manager.dart';
+export 'js_interop/helper.dart' show broadcastAppEvent;
+
+export 'dart:js_interop' show createJSInteropWrapper;
